03325cca4e9ec61eb5380cd7b390f2c15d769e60
mdricard <ricard@uta.edu>
2021-09-27 16:36:20 -0500

    added a new function

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def multiply_two(a, b):
+    return a * b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Homer J Simpson')
     print('Stop eating doughnuts they are bad for you')
     print('Lisa its your birthday')
+    print(multiply_two(3, 4))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
